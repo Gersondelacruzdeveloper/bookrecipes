dccf0d03fe7c65b5af4e0c317155f75a3e406e94
Gerson <qapuntacana@gmail.com>
2021-09-17 22:22:16 +0100

    fixed3

--- config/settings.py
+++ config/settings.py
@@ -33,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = 'eVYUKkSuIuU_CEqV7e04EoySG2O0tS1TWnbEwul293A'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = ['myrecipesbook1.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -187,13 +187,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "oucalculator@gmail.com"
 EMAIL_HOST_PASSWORD = 'wraaanoeenpeklry'
 
-SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
-SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS", default=2592000)
-SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
-SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
-SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
-CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID=AKIA54FAHMQPHYKCI4UN'
 AWS_SECRET_ACCESS_KEY = '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'
