acd794ee17b47edbdb9e3a12401382d969ae4545
Gerson <qapuntacana@gmail.com>
2021-09-19 13:04:59 +0100

    othet fiexed

--- config/settings.py
+++ config/settings.py
@@ -133,7 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
@@ -143,7 +142,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
 
 
 
