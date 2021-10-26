5954d2e52482b553f5935b849171e5077384b490
Gerson <qapuntacana@gmail.com>
2021-10-26 10:22:26 +0100

    change aws key

--- config/settings.py
+++ config/settings.py
@@ -31,10 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-"""
 DEBUG = env.bool("DEBUG", default=False)
-"""
+
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','www.allyourrecipes.co.uk','allyourrecipes.co.uk']
 
@@ -148,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 
-# django-crispy-forms
+#django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4' #new
 
 #CKEDITOR_UPLOAD_PATH
--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),  # new
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
